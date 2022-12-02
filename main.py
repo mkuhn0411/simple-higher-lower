9e8237e5fcf3dde04a2242e006422bff93c213d4
Mel <91561937+mkuhn0411@users.noreply.github.com>
2022-12-02 04:00:23 +0000

    Updated how random is handled

--- main.py
+++ main.py
@@ -5,26 +5,23 @@ from game_data import data
 from replit import clear
 print(logo)
 
-def shuffle_data():
-  return random.sample(data, len(data))
+def get_random():
+  return random.choice(data)
 
 def compare_text(name, desc, country, letter):
   print(f"Compare {letter}: {name}, {desc}, from {country}")
 
-def handle_guess(guess_data):
-  higher_count = max([guess_data[1]["follower_count"], guess_data[2]["follower_count"]])
-  highest_person = guess_data[1]["name"] if guess_data[1]["follower_count"] == higher_count else guess_data[2]["name"]
-  guess_person = guess_data[1]["name"] if guess_data[0] == 'A' else guess_data[2]["name"]
+def handle_guess(guess, comp_1, comp_2):
+  highest_person = comp_1["name"] if comp_1["follower_count"] > comp_2["follower_count"] else comp_2["name"]
+  guess_person = comp_1["name"] if guess == 'A' else comp_2["name"]
   return highest_person == guess_person
 
-def get_guess(data, ind):
-  comp_1 = data[ind]
-  comp_2 = data[ind + 1]         
+def get_guess(comp_1, comp_2):       
   compare_text(comp_1["name"],comp_1["description"], comp_1["country"],'A')
   print(vs) 
   compare_text(comp_2["name"],comp_2["description"], comp_2["country"],'B')
   guess = input("Who has more followers? Type 'A' or 'B': ")
-  return [guess, comp_1, comp_2]
+  return guess
 
 def print_right_text(score, guess_true):
   if guess_true:
@@ -33,19 +30,24 @@ def print_right_text(score, guess_true):
     print(f"Sorry, that's wrong. Final score: {score}")
 
 def run():
-  shuffled_data = shuffle_data()
-  index = 0
   score = 0
   game_active = True
+  comp_1 = get_random()
+  comp_2 = get_random() 
   
-  while game_active:
-    guess = get_guess(shuffled_data, index)
-    guess_true = handle_guess(guess)
+  while game_active: 
+    while comp_1 == comp_2:
+      comp_2 = get_random()
+    guess = get_guess(comp_1, comp_2)
+    guess_true = handle_guess(guess, comp_1, comp_2)
     if guess_true:
       score += 1
-      index += 2
+      clear()
       print_right_text(score, guess_true)
+      comp_1 = comp_2
+      comp_2 = random.choice(data)
     else: 
+      clear()
       print_right_text(score, guess_true)
       game_active = False
 
